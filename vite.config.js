8d869fef762a6966032d05892f7cf4533587f1d2
MYXXdev <myxxdev@myxxfm.com>
2025-07-25 20:02:52 +0200

    Switch base to custom domain root

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Pic-Nic-Torremolinos/', // Adjust if using relative base './'
+  base: './', 
 });
