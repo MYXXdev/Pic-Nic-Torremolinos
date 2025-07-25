4f9d8c7726613f593f3ce612dd2e2116f8acab0b
MYXXdev <myxxdev@myxxfm.com>
2025-07-25 17:37:07 +0200

    Fix base path and rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Because we will deploy with GitHub Actions to Pages and (optionally) a custom domain,
-// we can safely use '/' as base to avoid the white-page/base-path hell.
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Pic-Nic-Torremolinos/', // EXACTLY your GitHub repo name (case sensitive)
 });
